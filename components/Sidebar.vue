317e76c6ba65c7189afe1ac9fb7f4c8b9fba0163
shunta <exec.apea@gmail.com>
2020-02-24 17:46:04 +0700

    ADD: made a design base

---
+++ components/Sidebar.vue
@@ -0,0 +1,22 @@
+<template>
+  <b-menu>
+    <b-menu-list>
+      <b-menu-item
+        label="Expo"
+        icon="link"
+        tag="router-link"
+        target="_blank"
+        to="/expo"
+        class="has-text-white-ter"
+      >
+      </b-menu-item>
+    </b-menu-list>
+  </b-menu>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+@Component
+export default class Sidebar extends Vue {}
+</script>
+<style lang="scss" scoped></style>
