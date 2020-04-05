1c399f90ef8a2b28ddf2d6f968a93b0d14fd2650
shunta <exec.apea@gmail.com>
2020-04-05 15:22:47 +0900

    ADD: google font for Hero

--- components/Hero.vue
+++ components/Hero.vue
@@ -3,10 +3,10 @@
     <section class="hero has-background-white-ter has-text-centered">
       <div class="hero-body  hero-wrapper">
         <div class="container">
-          <h1 class="title is-size-2-mobile">
+          <h1 class="title is-size-2-mobile extra-font">
             Rin is fine
           </h1>
-          <h2 class="subtitle is-size-5-mobile">
+          <h2 class="subtitle is-size-5-mobile extra-font">
             Travel with me & aesthetic thing
           </h2>
         </div>
@@ -21,4 +21,9 @@ import { Component, Vue } from 'vue-property-decorator'
 export default class Hero extends Vue {}
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@import url('https://fonts.googleapis.com/css2?family=Caveat:wght@700&display=swap');
+.extra-font {
+  font-family: 'Caveat', cursive;
+}
+</style>
