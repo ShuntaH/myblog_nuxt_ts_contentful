5354826dd7adce3ff25412a7ae69c6a1bb204f6d
Shunta Hosaka <exec.apea@gmail.com>
2020-08-02 03:48:14 +0900

    ADD: layout/nothing.vue

---
+++ components/LoadingPage.vue
@@ -0,0 +1,22 @@
+<template>
+  <div></div>
+</template>
+
+<script lang="ts">
+  import { Component, Vue } from 'nuxt-property-decorator'
+
+  @Component({})
+  export default class extends Vue {@Component({
+    components: {},
+    computed: {
+      ...mapState(['series'])
+    },
+    layout: 'default'
+  })
+  export default class extends Vue {    name: 'LoadingPage'
+  }
+</script>
+
+<style scoped>
+
+</style>
