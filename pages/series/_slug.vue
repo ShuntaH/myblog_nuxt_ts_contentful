f8d5e378644f07f7de41eb342c06f77150950c5a
Shunta Hosaka <exec.apea@gmail.com>
2020-08-14 03:03:02 +0900

    UPDATE: stories list layout

--- pages/series/_slug.vue
+++ pages/series/_slug.vue
@@ -3,19 +3,27 @@
     <Hero
       :hero-title="this.series.fields.name"
       hero-subtitle="From 2018 to 2020"
-      hero-title-color="has-text-dark"
+      hero-title-color="has-text-black-bis"
       :hero-background-img-url="this.series.fields.thumbnail.fields.file.url"
     />
-    <h3
-      class="has-text-centered is-size-3 is-size-4-mobile py-4 has-text-weight-semibold"
-    >
-      {{ this.series.fields.name }}
-    </h3>
-    <div
-      v-for="(post, i) in seriesRelatedPosts(this.series.fields.name)"
-      :key="i"
-    >
-      <h4>{{ post.fields.title }}</h4>
+    <div class="wrapper">
+      <p class="is-size-3 is-size-4-mobile py-4 mt-5 has-text-weight-semibold">
+        All Stories
+      </p>
+      <ul>
+        <li
+          v-for="(post, i) in seriesRelatedPosts(this.series.fields.name)"
+          :key="i"
+          class="is-size-6 has-text-weight-semibold story-list"
+        >
+          <span class="pr-4">
+            {{ i + 1 }}
+          </span>
+          <h4 class="is-size-6">
+            <nuxt-link to="#">{{ post.fields.title }}</nuxt-link>
+          </h4>
+        </li>
+      </ul>
     </div>
   </div>
 </template>
@@ -48,4 +56,35 @@ export default class extends Vue {
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.story-list {
+  border-bottom: 0.5px solid whitesmoke;
+  display: flex;
+  justify-content: start;
+}
+ul {
+  margin-bottom: 60px;
+}
+@include pc() {
+  .wrapper {
+    width: 900px;
+    margin: 0 auto;
+  }
+}
+
+@include tablet() {
+  .wrapper {
+    width: 70%;
+    max-width: 900px;
+    margin: 0 auto;
+  }
+}
+
+@include sp() {
+  .wrapper {
+    width: 90%;
+    max-width: 410px;
+    margin: 0 auto;
+  }
+}
+</style>
